d1a3e5bd98583165d56a6f1166bd84e52db1b912
Maëla <maela.debrie@laposte.net>
2025-10-25 13:55:44 +0200

    Working swap script

--- Assets/Scripts/SwapHandler.cs
+++ Assets/Scripts/SwapHandler.cs
@@ -4,28 +4,23 @@ public class SwapHandler : MonoBehaviour
 {
     //Currently assigned via the Unity GUI
     public GameObject player;
-    public GameObject npc;
     
     public SpriteRenderer playerRender;
-    public SpriteRenderer npcRender;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //Get player & NPC sprites
         playerRender = player.GetComponent<SpriteRenderer>();
-        npcRender = npc.GetComponent<SpriteRenderer>();
     }
-    private GameObject FindClosestNPC(Vector3 playerPos) {
+    private GameObject FindClosestNPCPosition(Vector3 playerPos) {
         GameObject[] npcs = GameObject.FindGameObjectsWithTag("Swappable");
-        Debug.LogError($"Found {npcs.Length} NPCs with tag 'Swappable'");
         if (npcs.Length == 0)
             throw new System.Exception("No NPCs with tag 'Swappable' found in the scene.");
-        float shortestDistance = Mathf.Infinity;
+        float shortestDistance = float.MaxValue;
         GameObject closest = null;
         foreach (GameObject npc in npcs)
         {
-            Debug.LogError($"Checking NPC at position {npc.transform.position}");
             float distance = Vector3.Distance(playerPos, npc.transform.position);
             if (distance < shortestDistance)
             {
@@ -40,14 +35,15 @@ public class SwapHandler : MonoBehaviour
         // Get player & NPC position
         Vector3 playerPos = player.transform.position;
 
-        GameObject npc;
+        GameObject npc; 
         try {
-            npc = FindClosestNPC(playerPos);
+            npc = FindClosestNPCPosition(playerPos);
         } catch (System.Exception e) {
             Debug.LogError(e.Message);
             //Todo add visuals if no swap possible?
             return;
         }
+        SpriteRenderer npcRender = npc.GetComponent<SpriteRenderer>();
         Vector3 npcPos = npc.transform.position;
 
         // Set new positions
